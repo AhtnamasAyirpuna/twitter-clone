c5459f8efab9761468ecfb290dbfc2b287b7b98b
AhtnamasAyirpuna <samanthaanupriya98@gmail.com>
2025-10-24 21:29:49 +0800

    added postsSlice.js

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,38 @@
+import {createAsyncThunk, createSlice} from '@reduxjs@toolkit';
+
+const BASE_URL = 'api url';
+
+//Async thunk for fetching user's posts
+export const fetchPostsByUser = createAsyncThunk(
+    "posts/fetchByUser",
+    async (userId) => {
+        const response = await fetch (`${BASE_URL}/posts/user/${userId}`);
+        return response.json(); // this is the action in the addCase
+        //return [{id: 1, content: "Hello"}]
+    }
+);
+
+//Slice
+const postsSlice = createSlice({
+    name: "posts",
+    initialState: {posts: [], loading: true},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchPostsByUser.fulfilled, (state,action) => {
+            //action payload = [{id: 1, content: "Hello"}]
+            // we got it from fetchPostsByUSer output
+            state.posts = action.payload;
+            //state.posts = [] ; it is the current posts you are showing
+            
+            //since action payload is returned
+            // state.posts = [{id: 1, content: "Hello"}]
+
+            //before: state.loading = true
+            //we want the loading animation to stop
+            state.loading = false; // to stop the loding animation
+        });
+    },
+});
+
+export default postsSlice.reducer;
+
