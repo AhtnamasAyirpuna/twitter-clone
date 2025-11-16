0c5075a612d6bc7b19b4cbf4b1c74f3141d35253
AhtnamasAyirpuna <samanthaanupriya98@gmail.com>
2025-11-16 13:41:32 +0800

    implement like and dislike in Firestore

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -48,6 +48,52 @@ export const savePost = createAsyncThunk(
   }
 );
 
+export const likePost = createAsyncThunk(
+    "posts/likePost",
+    async({userId, postId}) => {
+        try {
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+
+            const docSnap = await getDoc(postRef);
+
+            if (docSnap.exists()) {
+                const postData = docSnap.data();
+                const likes = [...postData.likes, userId];
+
+                await setDoc(postRef, {...postData, likes});
+            }
+
+            return {userId, postId};
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+);
+
+export const removeLikeFromPost = createAsyncThunk(
+    "posts/removeLikeFromPost",
+    async({userId, postId}) => {
+        try {
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+
+            const docSnap = await getDoc(postRef);
+
+            if (docSnap.exists()) {
+                const postData = docSnap.data();
+                const likes = postData.likes.filter((id) => id !== userId);
+
+                await setDoc(postRef, {...postData, likes});
+            }
+
+            return {userId, postId};
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+);
+
 //Slice
 const postsSlice = createSlice({
     name: "posts",
@@ -60,6 +106,26 @@ const postsSlice = createSlice({
         })
         .addCase(savePost.fulfilled, (state, action) => {
             state.posts = [action.payload, ...state.posts];
+        })
+        .addCase(likePost.fulfilled, (state, action) => {
+            const {userId, postId} = action.payload;
+
+            const postIndex = state.posts.findIndex((post) => post.id === postId);
+
+            if (postIndex !== -1) {
+                state.posts[postIndex].likes.push(userId);
+            }
+        })
+        .addCase(removeLikeFromPost.fulfilled, (state, action) => {
+            const {userId, postId} = action.payload;
+
+            const postIndex = state.posts.findIndex((post) => post.id === postId);
+
+            if (postIndex !== -1) {
+                state.posts[postIndex].likes = state.posts[postIndex].likes.filter(
+                    (id) => id !== userId
+                );
+            }
         });
     },
 });
